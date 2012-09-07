d6e5f0f4630404f59c5c6b1a99360d3226658741
Lutz Hain <source@joolabs.de>
2012-09-07 10:03:27 +0200

    1.0

---
+++ mod_joojqmenu.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package		Joomla.Site
+ * @subpackage	mod_menu
+ * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+// Include the syndicate functions only once
+require_once dirname(__FILE__).'/helper.php';
+
+$list	= modJQMenuHelper::getList($params);
+$app	= JFactory::getApplication();
+$menu	= $app->getMenu();
+$active	= $menu->getActive();
+$active_id = isset($active) ? $active->id : $menu->getDefault()->id;
+$path	= isset($active) ? $active->tree : array();
+$showAll	= $params->get('showAllChildren');
+$class_sfx	= htmlspecialchars($params->get('class_sfx'));
+
+// include css and jquery
+$doc =& JFactory::getDocument();
+$doc->addStyleSheet( JURI::root().'modules/mod_joojqmenu/css/joojqmenu.css' );
+$doc->addScript( 'http://code.jquery.com/jquery-1.7.2.min.js' );
+?>
+<?php
+if(count($list)) {
+	require JModuleHelper::getLayoutPath('mod_joojqmenu', $params->get('layout', 'default'));
+}
